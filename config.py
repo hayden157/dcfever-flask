27ebb9f4f57b659371801c26851db03f7bb9e9c6
hayden157 <57666622+hayden157@users.noreply.github.com>
2025-04-08 01:50:17 +0800

    Add config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://postgres:postgres@db:5432/dcfever'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 10
+    UPLOAD_FOLDER = os.path.join(basedir, 'app/static/uploads')
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
